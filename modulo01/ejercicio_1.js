b323145c8d5809bdd6887324304b0218219dc056
Menciajp <80480967+Menciajp@users.noreply.github.com>
2023-06-13 17:32:51 -0300

    Add files via upload

---
+++ modulo01/ejercicio_1.js
@@ -0,0 +1,2 @@
+document.writeln("Hola Mundo!");
+//escribe Hola Mundo! en la pagina web
---
+++ modulo01/ejercicio_6.js
@@ -0,0 +1,5 @@
+let dias = ["lunes","martes","miercoles","jueves","viernes","sabado", "domingo"];
+
+for (const dia of dias) {
+   console.log(dia); 
+}
